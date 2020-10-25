8cc269dc33705801a747c995b510f0d8f22ed83e
balinth0rvath <horvath0balint@gmail.com>
2020-10-25 12:14:55 +0100

    Convert cdt project to make project

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PATH=$PATH:/opt/st/stm32cubeide_1.4.0/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.7-2018-q2-update.linux64_1.4.0.202007081208/tools/bin/
+export PATH=/opt/st/stm32cubeide_1.4.0/plugins/com.st.stm32cube.ide.mcu.externaltools.make.linux64_1.4.0.202007081208/tools/bin:/opt/st/stm32cubeide_1.4.0/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.7-2018-q2-update.linux64_1.4.0.202007081208/tools/bin/:$PATH
 export CC=/opt/st/stm32cubeide_1.4.0/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.7-2018-q2-update.linux64_1.4.0.202007081208/tools/bin/arm-none-eabi-gcc
 #/opt/st/stm32cubeide_1.4.0/plugins/com.st.stm32cube.ide.mcu.externaltools.make.linux64_1.4.0.202007081208/tools/bin
